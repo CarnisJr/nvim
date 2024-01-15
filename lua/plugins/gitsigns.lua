6b21d7f0e41e6cd5b98f5175d95563d8cc02b774
CarnisJr <mirandab950@gmail.com>
2024-01-15 09:53:15 -0500

    add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,13 @@
+return{
+  "lewis6991/gitsigns.nvim",
+   opts = {
+     numhl = true,
+     max_file_length = 10000,
+
+   } 
+-- config = true,
+-- Es exactemente lo mismo que config = true
+--  config = fuction ()
+-- 	require("gitsigns").setup()
+-- end
+}
