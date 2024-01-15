9d6fce46ecf00ab2c40e436067c617edd7d6ea12
CarnisJr <mirandab950@gmail.com>
2024-01-15 10:56:44 -0500

    tab fixed

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,10 @@
 return{
   "lewis6991/gitsigns.nvim",
-   opts = {
-     numhl = true,
-     max_file_length = 10000,
+  opts = {
+    numhl = true,
+    max_file_length = 10000,
 
-   } 
+  } 
 -- config = true,
 -- Es exactemente lo mismo que config = true
 --  config = fuction ()
